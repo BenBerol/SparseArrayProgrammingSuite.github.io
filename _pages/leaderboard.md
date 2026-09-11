7952fc4ad175ddf896610ca1db21f15b4534575b
Ben Berol <benberol40@gmail.com>
2026-09-11 17:03:17 -0400

    Port observable framework data vis

--- _pages/leaderboard.md
+++ _pages/leaderboard.md
@@ -4,3 +4,5 @@ permalink: /leaderboard/
 layout: single
 author_profile: false
 ---
+
+[Interactive performance profile]({{ '/performance/' | relative_url }})
---
+++ performance/observablehq.config.js
@@ -0,0 +1,6 @@
+// See https://observablehq.com/framework/config for documentation.
+export default {
+  title: "SAPS Benchmarks",
+  root: "docs",
+  base: "/performance",
+};
